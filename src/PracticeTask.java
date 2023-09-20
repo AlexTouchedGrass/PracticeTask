9ea589ec62b3c554b29a778884f006b5927b1cbd
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-20 09:31:01 -0400

    Practice for

--- src/PracticeTask.java
+++ src/PracticeTask.java
@@ -1,5 +1,23 @@
-public class Main {
+import java.util.Scanner;
+
+public class PracticeTask {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int age = 17;
+        double gpa = 4;
+
+        System.out.println("Enter your age then gpa: ");
+
+        if (scan.hasNextInt ()) {
+            age = scan.nextInt();
+            gpa = scan.nextInt();
+        }
+        else{
+            System.out.println("You have entered an invalid value for age or gpa.");
+            System.exit(0);
+        }
+
+        System.out.println("Your age is " + age + " years old. You have a " + gpa + " GPA.");
     }
 }
